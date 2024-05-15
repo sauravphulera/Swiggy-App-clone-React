2cb7686b1b8760278a7aa898590695616b15efe6
sauravphulera <phulerasanju20@gmail.com>
2024-05-15 17:03:41 +0530

    episode-2 (Igniting the app)

--- Readme.md
+++ Readme.md
@@ -1,2 +1,21 @@
 #### Namaste React
 # Episode wise dev and notes of Namaste react projects
+
+
+# parcel
+- DEv Build
+- Local Server
+- HMR -> Hot module replacements
+- File watching Agorithm
+- Caching - Faster builds
+- Image optimization
+- Minification of file in prod built
+- Bundling
+- Compress
+- Consistent Hashing (keeps track of ...)
+- Code Splitting
+- Differential Bundling - for different browser it bundles such that the code runs smoothly on older versions.
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
 	React.createElement("div", { id: "child" }, [
-	  React.createElement("h1", {}, "I'm an h1 tag"),
-	  React.createElement("h2", {}, "I'm an h2 tag"),
+	  React.createElement("h1", {}, "I'm an h1 tag with some data"),
+	  React.createElement("h2", {}, "I'm an h2 tag with"),
 	]),
 	React.createElement("div", { id: "child2" }, [
-	  React.createElement("h1", {}, "I'm an h1 tag"),
-	  React.createElement("h2", {}, "I'm an h2 tag"),
+	  React.createElement("h1", {}, "I'm an h1 tag with some"),
+	  React.createElement("h2", {}, "haha I am also an h2 tag"),
 	]),
   ]);
   
